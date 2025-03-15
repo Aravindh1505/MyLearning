eb52d2c720cbcabba4515710570dd7b3c0e054a2
Aravindh1505 <aravindh.samidurai@gmail.com>
2025-03-15 10:56:26 +0800

    Auth module initial commit

---
+++ app/src/main/java/com/aravindh/mylearning/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.aravindh.mylearning.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.aravindh.mylearning.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
